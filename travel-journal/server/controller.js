bf516cd570205a61527bdc75236cba85943dc214
daxtonparke <daxtonparke@gmail.com>
2021-12-10 11:37:11 -0700

    q5

--- travel-journal/server/controller.js
+++ travel-journal/server/controller.js
@@ -1,6 +1,33 @@
+require('dotenv').config()
+const {CONNECTION_STRING} = process.env
+const Sequelize = require('sequelize')
 
+const sequelize = new Sequelize (CONNECTION_STRING, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+  })
 
 module.exports = {
+    getCities: (req,res) =>{
+sequelize.query(``)
+    },
+    createCity: (req,res) =>{
+        const {name,rating,countryId} = req.body
+        sequelize.query(`INSERT INTO cities (name, rating, country_id)
+            VALUES ('${name}', ${rating}, ${countryId});`) 
+            .then(dbRes => res.status(200).send(dbRes[0]))
+            .catch(err=>console.log(err))
+
+    },
+    getCountries: (req,res) =>{
+        sequelize.query(`SELECT * FROM countries;`)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
+    },
     seed: (req, res) => {
         sequelize.query(`
             drop table if exists cities;
@@ -8,10 +35,15 @@ module.exports = {
 
             create table countries (
                 country_id serial primary key, 
-                name varchar
+                name VARCHAR(50)
             );
 
-            *****YOUR CODE HERE*****
+            CREATE TABLE cities(
+                city_id SERIAL PRIMARY KEY,
+                name VARCHAR(50),
+                rating INT,
+                country_id INT NOT NULL REFERENCES countries(country_id)
+            );
 
             insert into countries (name)
             values ('Afghanistan'),
